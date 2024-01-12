e104c9a528865bdbc722f2490e9fe31285971ec4
George Stanley <UF19@greatdentalplans.com>
2024-01-12 16:22:25 -0600

    Initial Commit

---
+++ RickyAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyAndMorty
+//
+//  Created by Zill-e-Abbas on 12.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
